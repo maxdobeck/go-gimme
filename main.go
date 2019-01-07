3c6265ae7172cae7c4483304eb296a6081603fe9
Max Dobeck <msdobeck@gmail.com>
2019-01-06 22:46:28 -0800

    Testing mod operator in preparation for spinner.

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+
 func main() {
+	const spinner = `. .. ... .... .....`
 	fmt.Println("Hello World")
+	i := 0
+	for {
+		fmt.Println(i%5) 
+		i++
+		if i == 5 {
+			break
+		}
+	}
 }
