935880eb49ba8b81fa077debffc7cabe621e46a4
Max Dobeck <msdobeck@gmail.com>
2019-01-13 22:54:32 -0800

    Added emails command within the main package as an example of how to call a function after a flag has been called by the user.

---
+++ emails.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func findEmails() {
+	fmt.Println("finding your emails...")
+}
--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import "flag"
 func main() {
 	fmt.Println("welcome to gimme.  Use -h to get help.")
 	emails := flag.Bool("emails", false, "Find all emails.  Defaults to false.")
-
 	flag.Parse()
+	run(emails)
+}
 
-	fmt.Println(*emails)
+func run(emails *bool) {
+	if *emails == true {
+		findEmails()
+	}
 }
