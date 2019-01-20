a4564ccf974e3a6f76b436eefc8a93cfd2c93c96
Max Dobeck <msdobeck@gmail.com>
2019-01-19 21:03:01 -0800

    Added basic input check and modified the control flow in main to be less complex.

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/maxdobeck/gimme
+
+require github.com/atotto/clipboard v0.1.1
--- main.go
+++ main.go
@@ -5,16 +5,14 @@ import "flag"
 
 func main() {
 	emails := flag.Bool("emails", false, "Find all emails.  Defaults to false.")
+	input := flag.String("input", "clipboard", "Specify the input file.  Defaults to your clipboard.")
 	flag.Parse()
-	if *emails {
-		run(emails)
+
+	if *input == "clipboard" {
+		parseInput()
+	} else if *emails == true {
+		findEmails()
 	} else {
 		fmt.Println("gimme: Use -h to get help.")
-	}
-}
-
-func run(emails *bool) {
-	if *emails == true {
-		findEmails()
 	}
 }
