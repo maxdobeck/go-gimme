305ef9db70fa862693a27c587e3cab6c464a0db7
Max Dobeck <msdobeck@gmail.com>
2019-01-19 21:02:04 -0800

    Added basic function to get clipboard contents.

---
+++ input.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/atotto/clipboard"
+)
+
+func parseInput() {
+	content, err := clipboard.ReadAll()
+	if err != nil {
+		fmt.Println("Problem reading from your clipboard\n: ", err)
+	}
+	fmt.Println("clipboard contents: ", content)
+}
